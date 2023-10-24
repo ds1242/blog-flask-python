c6158b7b0a350a42c5b01e3d9d85e988928f51ab
David Shaw <dshaw11@utk.edu>
2023-10-24 11:23:48 -0400

    fetched api data for the post preview data and got it to load on the home screen, made sample post a route and page to keep the sample post example

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
-
+from post import Post
+import requests
 # setting up the initial flask sever
 app = Flask(__name__)
+
+posts = requests.get('https://api.npoint.io/2fe1152e54348485537b').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"], post["date"], post['author'])
+    post_objects.append(post_obj)
+
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=post_objects)
 
 
 @app.route("/about")
@@ -17,6 +25,21 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/sample-post")
+def contact():
+    return render_template("sample-post.html")
+
+
+@app.route('/post/<int:num>')
+def post(num):
+    request_post = None
+    for blog_post in post_objects:
+        if blog_post.id == num:
+            request_post = blog_post
+    return render_template("post.html", post_num=num, blog_post=request_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ post.py
@@ -0,0 +1,8 @@
+class Post:
+    def __init__(self, post_id, title, subtitle, text, date, author):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = text
+        self.date = date
+        self.author = author
